8b4239615b683be737f7bdd51a2d1d34101f9ddb
Joshua Miles <joshua.m@hackerusa.com>
2020-09-30 09:33:50 -0500

    added solution

--- index.js
+++ index.js
@@ -6,3 +6,36 @@ const images = [
     './assets/5.jpg',
     './assets/6.jpg'
 ]
+
+
+const image = document.querySelector('.image')
+const nextButton = document.querySelector('.next')
+const previousButton = document.querySelector('.previous')
+const bubbleButtons = document.querySelectorAll('.bubble')
+
+let currentImageIndex = 0
+image.src = images[currentImageIndex]
+bubbleButtons[currentImageIndex].textContent = '●'
+
+for (let i = 0; i < bubbleButtons.length; i++) {
+    bubbleButtons[i].addEventListener('click', function (e) {
+        currentImageIndex = i;
+        image.src = images[currentImageIndex]
+        for(let i = 0; i < bubbleButtons.length; i++) bubbleButtons[i].textContent = '○'
+        bubbleButtons[currentImageIndex].textContent = '●'
+    })
+}
+
+previousButton.addEventListener('click', function () {
+    if (currentImageIndex > 0) currentImageIndex--
+    image.src = images[currentImageIndex]
+    for(let i = 0; i < bubbleButtons.length; i++) bubbleButtons[i].textContent = '○'
+    bubbleButtons[currentImageIndex].textContent = '●'
+})
+
+nextButton.addEventListener('click', function () {
+    if (currentImageIndex < 5) currentImageIndex++
+    image.src = images[currentImageIndex]
+    for(let i = 0; i < bubbleButtons.length; i++) bubbleButtons[i].textContent = '○'
+    bubbleButtons[currentImageIndex].textContent = '●'
+})
